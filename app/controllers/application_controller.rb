ee2f9fce88345afd637a5e1f6c1349b7ba49b7e6
yusukeee811 <132319174+yusukeee811@users.noreply.github.com>
2023-05-13 14:22:06 +0000

    [Finisih]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
-  
+  before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
+    post_images_path
+  end
+  
+  def after_sign_out_path_for(resource)
     about_path
   end
 
